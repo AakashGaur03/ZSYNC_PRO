19acd0346e738e336d2f0c17659b2c03afa89971
AakashGaur03 <aakash.sunrise23@gmail.com>
2023-12-19 00:11:49 +0530

    Updated Navbar
    Added Setting Section in Navbar
    and Clock Madal in it

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,8 +1,27 @@
-import { Container, Nav, Navbar, Form } from "react-bootstrap";
+import {
+  Container,
+  Nav,
+  NavDropdown,
+  Navbar,
+  Form,
+  Button,
+  Modal,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { IoSettings, IoSettingsOutline } from "react-icons/io5";
+import { FaRegClock, FaTrash } from "react-icons/fa";
+import { useState } from "react";
+
+import "../NavbarCSS.css";
+
 const NavbarComp = ({ theme, onToggleClick, themeSwitch }) => {
+
   const textColorClass = theme === "Light" ? "text-black" : "text-white";
   const navbarColorClass = theme === "Light" ? "bg-light" : "bg-dark";
+
+  const [showClockModal, setShowClockModal] = useState(false);
+  const handleCloseClockModal=()=>setShowClockModal(false);
+  const handleShowClockModal=()=>setShowClockModal(true);
 
   return (
     <>
@@ -34,7 +53,29 @@ const NavbarComp = ({ theme, onToggleClick, themeSwitch }) => {
                 label={`${theme} Mode `}
               />
             </Form>
+            <NavDropdown
+              title={
+                theme === "Light" ? (
+                  <IoSettings size="30px" />
+                ) : (
+                  <IoSettingsOutline size="30px"/>
+                )
+              }
+              id="settingDropdown"
+            >
+              <NavDropdown.Item className={`${navbarColorClass} ${textColorClass}`}  onClick={handleShowClockModal}><FaRegClock size="20px" className="me-3"/>Clock</NavDropdown.Item>
+              <NavDropdown.Item className={`${navbarColorClass} ${textColorClass}`}><FaTrash size="20px" className="me-3" />Recently Deleted</NavDropdown.Item>
+            </NavDropdown>
           </Navbar.Collapse>
+          <Modal show={showClockModal} onHide={handleCloseClockModal} centered>
+            <Modal.Header>Hi</Modal.Header>
+            <Modal.Body>Hi</Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={handleCloseClockModal}>Click Me</Button>
+              <Button variant="success" onClick={handleCloseClockModal}>Save Me</Button>
+            </Modal.Footer>
+
+          </Modal>
         </Container>
       </Navbar>
     </>
