c1858c1744a4cf14559301a8c425ba1e1d754044
AakashGaur03 <aakash.sunrise23@gmail.com>
2023-12-28 07:45:59 +0530

    Updated Task Status and Deletion checkbox and icon

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,11 +6,36 @@ import { FaEdit } from "react-icons/fa";
 const Todo = () => {
   const [newTask, setNewTask] = useState("");
   const [tasks, setTasks] = useState([]);
+
   const addTask = () => {
     if (newTask.trim() !== "") {
-      setTasks([...tasks, { id: Date.now(), text: newTask, status: false }]);
+      setTasks([{ id: Date.now(), text: newTask, completed: false }, ...tasks]);
+      setNewTask("");
     }
-    setNewTask("");
+  };
+  const toggleTaskStatus = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+  const removeTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+  // const editTask =(taskId)=>{
+  //   const task = tasks.find((task)=>task.id === taskId)
+  //   if(task)
+  //   {
+  //     setNewTask(task.text)
+  //   }
+  // }
+  const editTask = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, text: newTask } : task
+    );
+    setTasks(updatedTasks);
+    setNewTask(""); // Clear the input field after editing
   };
   return (
     <>
@@ -35,12 +60,29 @@ const Todo = () => {
         {tasks.map((task) => (
           <li key={task.id} style={{ display: "flex" }}>
             <div style={{ width: "80%" }}>
-              {`${task.id} : `}
-              {task.text}
+              <span
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                }}
+              >
+                {task.text}
+              </span>
             </div>
-            <Form.Check className="me-3" />
-            <FaEdit size={20} className="me-3" />
-            <FaTrash size={20} className="me-3" />
+            <Form.Check
+              className="me-3"
+              checked={task.completed}
+              onChange={() => toggleTaskStatus(task.id)}
+            />
+            <FaEdit
+              size={20}
+              className="me-3"
+              onClick={() => editTask(task.id)}
+            />
+            <FaTrash
+              size={20}
+              className="me-3 cursorPointer"
+              onClick={() => removeTask(task.id)}
+            />
           </li>
         ))}
       </ul>
