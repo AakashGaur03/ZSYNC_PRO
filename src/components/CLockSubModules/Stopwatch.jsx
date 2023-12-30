94ec2211956c1cbc476894e9cfcba7187c5cff01
AakashGaur03 <aakash.sunrise23@gmail.com>
2023-12-31 00:06:45 +0530

    Updated Incodnito Todo and Stopwatch
    Added incognito Todo and in stopwatch added stopwatch functionalities

--- src/components/CLockSubModules/Stopwatch.jsx
+++ src/components/CLockSubModules/Stopwatch.jsx
@@ -1,64 +1,100 @@
-// import React, { useState } from 'react'
-// import { Button } from 'react-bootstrap';
-
-// const Stopwatch = () => {
-//     // const a=0;
-//     const [a,setA]=useState(0)
-//     const add=()=>{
-//         setInterval(() => {
-//             setA(prev => prev +1)
-
-//         }, 10);
-//     }
-//   return (
-//     <>
-//         <div>Stopwatch</div>
-//         <div>{a}</div>
-//         <Button onClick={add}/>
-//     </>
-//   )
-// }
-
-// export default Stopwatch
-
-import React, { useRef, useState } from "react";
-import { Button } from "react-bootstrap";
+import React, { useEffect, useRef, useState } from "react";
+import { VscDebugStart } from "react-icons/vsc";
+import { IoIosPause } from "react-icons/io";
+import { GrPowerReset } from "react-icons/gr";
+import { FaRegFlag } from "react-icons/fa";
 
 const Stopwatch = () => {
   const [elapsedTime, setElapsedTime] = useState(0);
-  const intervalRef = useRef()
+  const [flagElapsedTime, setFlagElapsedTime] = useState(0);
+  const [isActiveStopwatch, setIsActiveStopwatch] = useState(false);
+  const [isStopwatchStart, setIsStopwatchStart] = useState(false);
+  const [flagElapsedTimeArr, setFlagElapsedTimeArr] = useState([]);
+  const intervalRef = useRef();
   const formatTime = (time) => {
     const miliSeconds = time % 1000;
     const seconds = Math.floor(time / 1000) % 60;
     const minutes = Math.floor(time / (1000 * 60)) % 60;
     const hours = Math.floor(time / (1000 * 60 * 60)) % 60;
+    const formatNumber = (num) => (num < 10 ? `0${num}` : num);
+    const formattedMilliseconds = formatNumber(Math.floor(miliSeconds / 10));
 
-    const formatNumber=(num)=>(num<10 ? `0${num}`:num)
-    const formattedMilliseconds =formatNumber(Math.floor(miliSeconds/10))
+    return `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(
+      seconds
+    )}:${formattedMilliseconds}`;
+  };
 
-    return `${formatNumber(hours)}:${formatNumber(minutes)}:${formatNumber(seconds)}:${formattedMilliseconds}`
-  };
-  const handleStartStopwatch = () => {
-    intervalRef.current = setInterval(() => {
-        setElapsedTime((prev) => prev+10)
-    }, 10);
-  };
-  const handleStopStopwatch = () => {
-    clearInterval(intervalRef.current)
+  useEffect(() => {
+    if (isActiveStopwatch) {
+      intervalRef.current = setInterval(() => {
+        setElapsedTime((prev) => prev + 10);
+        setFlagElapsedTime((prev) => prev + 10);
+      }, 10);
+    } else {
+      clearInterval(intervalRef.current);
+    }
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, [isActiveStopwatch]);
+  const handleToggleStopwatch = () => {
+    setIsActiveStopwatch(!isActiveStopwatch);
+    setIsStopwatchStart(true);
   };
   const handleResetStopwatch = () => {
-    clearInterval(intervalRef.current)
-    setElapsedTime(0)
+    clearInterval(intervalRef.current);
+    setElapsedTime(0);
+    setIsActiveStopwatch(false);
+    setIsStopwatchStart(false);
+    setFlagElapsedTime(0)
+    setFlagElapsedTimeArr([]);
   };
+  const flagTime = () => {
+    console.log(formatTime(flagElapsedTime), formatTime(elapsedTime));
+    setFlagElapsedTimeArr((prevFlagElapsedTimeArr) => [
+      ...prevFlagElapsedTimeArr,
+      { flagElapsedTime, elapsedTime },
+    ]);
+    console.log(flagElapsedTimeArr, " aaa");
+    setFlagElapsedTime(0);
+  };
+
   return (
-    <>
-      <div>Stopwatch</div>
-      <div>{formatTime(elapsedTime)}</div>
-      <Button onClick={handleStartStopwatch}>Start</Button>
-      <Button onClick={handleStopStopwatch}>Stop</Button>
-      <Button onClick={handleResetStopwatch}>Reset</Button>
-    </>
+    <div className="stopwatch-container mb-2">
+      <div className="stopwatch mb-4">
+        <span>{formatTime(elapsedTime)}</span>
+      </div>
+      <div className="d-flex w-100 justify-content-around">
+        {isStopwatchStart && (
+          <GrPowerReset
+            className="stopwatchIcons"
+            onClick={handleResetStopwatch}
+            size={30}
+          />
+        )}
+        <div className="stopwatchIcons" onClick={handleToggleStopwatch}>
+          {isActiveStopwatch ? (
+            <IoIosPause size={30} />
+          ) : (
+            <VscDebugStart size={30} />
+          )}
+        </div>
+        {isStopwatchStart && (
+          <FaRegFlag onClick={flagTime} className="stopwatchIcons" size={30} />
+        )}
+      </div>
+      {flagElapsedTimeArr
+        .slice()
+        .reverse()
+        .map((x, index) => (
+          <div key={index} className="d-flex w-100 justify-content-around">
+            <div>{flagElapsedTimeArr.length - index}</div>
+            <div>{formatTime(x.flagElapsedTime)}</div>
+            <div>{formatTime(x.elapsedTime)}</div>
+          </div>
+        ))}
+    </div>
   );
 };
-
 export default Stopwatch;
--- src/components/IncognitoTodo.jsx
+++ src/components/IncognitoTodo.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import { FaTrash ,FaEdit } from "react-icons/fa";
 
-const IncognitoTodo = () => {
+const Todo = () => {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState(JSON.parse(sessionStorage.getItem("taskArray"))?JSON.parse(sessionStorage.getItem("taskArray")):[]);
+  const [editingTaskId,setEditingTaskId]=useState(null)
+
+  useEffect(() => {
+    const storedTask = JSON.parse(sessionStorage.getItem("taskArray")) || []
+    setTasks(storedTask);
+  },[]);
+
+  useEffect(()=>{
+    sessionStorage.setItem("taskArray",JSON.stringify(tasks));
+  },[tasks])
+
+  const addTask = () => {
+    if (newTask.trim() !== "") {
+      setTasks([{ id: Date.now(), text: newTask, completed: false }, ...tasks]);
+      setNewTask("");
+    }
+  };
+  const toggleTaskStatus = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, completed: !task.completed } : task
+    );
+    setTasks(updatedTasks);
+  };
+  const removeTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+  const editTask = (taskId) => {
+    setEditingTaskId(taskId)
+    const taskToEdit = tasks.find((task)=>task.id===taskId)
+    if(taskToEdit)
+    {
+      setNewTask(taskToEdit.text)
+    }
+  };
+  const saveEditing =()=>{
+    const updatedTask = tasks.map((task)=>task.id===editingTaskId? {...task , text:newTask} : task )
+    setTasks(updatedTask)
+    setEditingTaskId(null)
+    setNewTask("")
+  }
   return (
-    <div>Incognito Todo</div>
-  )
-}
+    <>
+      <div>Todo</div>
+      <InputGroup className="mb-3">
+        <Form.Control
+          type="text"
+          value={newTask}
+          onChange={(e) => {
+            setNewTask(e.target.value);
+          }}
+          placeholder="Add Task"
+          aria-label=""
+          aria-describedby="addTask"
+        />
+        <Button variant="primary" id="" onClick={editingTaskId? saveEditing :addTask}>
+          {editingTaskId ? "Save Task" :"Add Task"}
+        </Button>
+      </InputGroup>
+      {/* <div>{newTask}</div> */}
+      <ul style={{ overflowY: "auto", maxHeight: "75vh" }}>
+        {tasks.map((task) => (
+          <li key={task.id} style={{ display: "flex" }}>
+            <div style={{ width: "80%" }}>
+              <span
+                style={{
+                  textDecoration: task.completed ? "line-through" : "none",
+                }}
+              >
+                {task.text}
+              </span>
+            </div>
+            <Form.Check
+              className="me-3"
+              checked={task.completed}
+              onChange={() => toggleTaskStatus(task.id)}
+            />
+            <FaEdit
+              size={20}
+              className="me-3"
+              onClick={() => editTask(task.id)}
+            />
+            <FaTrash
+              size={20}
+              className="me-3 cursorPointer"
+              onClick={() => removeTask(task.id)}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default IncognitoTodo
+export default Todo;
