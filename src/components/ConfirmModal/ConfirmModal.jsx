455f09b11b85d90b2b1ecfa69510144e287bac7b
AakashGaur03 <aakash.sunrise23@gmail.com>
2024-01-28 20:01:19 +0530

    Adding Confirm Modal

---
+++ src/components/ConfirmModal/ConfirmModal.jsx
@@ -0,0 +1,28 @@
+import { Modal, Button } from "react-bootstrap";
+import { useState } from "react";
+const ConfirmModal = () => {
+  const [showconfirmModal, setshowConfirmModal] = useState(false);
+  const handleshowConfirmModalClose = () => setshowConfirmModal(false);
+  const handleshowConfirmModalShow = () => setshowConfirmModal(true);
+  return (
+    <>
+      <Button variant="secondary" onClick={handleshowConfirmModalShow}>
+        Show modal
+      </Button>
+
+      <Modal show={showconfirmModal} onHide={handleshowConfirmModalClose}>
+        <Modal.Body>Are you Sure You want to delete</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleshowConfirmModalClose}>
+            Yes
+          </Button>
+          <Button variant="primary" onClick={handleshowConfirmModalClose}>
+            No
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default ConfirmModal;
